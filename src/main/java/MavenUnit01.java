be5ae2502232f8fe23fead5e84af7663001e3532
Malusi Pakade <pakadem@gmail.com>
2020-02-26 16:18:48 +0200

    initial files

---
+++ src/main/java/MavenUnit01.java
@@ -0,0 +1,12 @@
+public class MavenUnit01 {
+    public static void main (String [] args){
+        System.out.println("Print this maven unit");
+        System.out.println(addition(3,5));
+    }
+    public static  int addition (int x, int y){
+        x = x;
+        y = y;
+
+        return x + y;
+    }
+}
---
+++ src/test/java/MavenUnit01Test.java
@@ -0,0 +1,26 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+public class MavenUnit01Test {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void main() {
+    }
+
+    @Test
+    public void addition() {
+        Assert.assertEquals(8, MavenUnit01.addition(3,5));
+    }
+}
